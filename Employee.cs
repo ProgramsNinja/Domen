280aad4d066a58701b37b756bdb9d671f492afa6
egor4ka <135698192+egor4ka@users.noreply.github.com>
2024-12-20 00:46:24 +0200

    add employee

--- Employee.cs
+++ Employee.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace Domen
 {
-    internal class Employee
+    public class Employee
     {
+        private Employee(Guid id, string name, Guid companyId, Guid roleId)
+        {
+            Id = id;
+            Name = name;
+            CompanyId = companyId;
+            RoleId = roleId;
+        }
+
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public Guid CompanyId { get; private set; }
+        public Guid RoleId { get; private set; }
+
+        public static Employee Create(string name, Guid roleId, Guid companyId)
+        {
+            ArgumentNullException.ThrowIfNull(name);
+            return new Employee(new Guid(), name, companyId, roleId);
+        }
     }
 }
