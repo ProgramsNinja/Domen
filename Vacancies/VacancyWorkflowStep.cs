b2917a6c563626f189b10d699f2d40344e94cca5
Дима Чумаченко <dmitriy.chumachenko.00@mail.ru>
2024-12-20 00:42:08 +0200

    добавил класс VacancyWorkflowStep

--- Vacancies/VacancyWorkflowStep.cs
+++ Vacancies/VacancyWorkflowStep.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domen.Candidates;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace Domen.Vacancies
 {
     public class VacancyWorkflowStep
     {
+        public Guid UserId { get; private set; }
+        public Guid RoleId { get; private set; }
+        public string Description { get; private set; }
+        public int StepNumber { get; private set; }
+
+        public CandidateWorkflowStep Create()
+        {
+            return CandidateWorkflowStep.Create(this);
+        }
     }
 }
